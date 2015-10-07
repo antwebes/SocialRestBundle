6000d6970d3a9f65530c03afbf745d1f7352a473
Xabier Fernández <jjbier@gmail.com>
2015-10-07 13:18:01 +0200

    update bootstrap.php for used in symfony project or library

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,20 @@
 <?php
-$autoloadFile = __DIR__ . '/../../../../../../vendor/autoload.php';
+/*
+ * This file is part of the SocialRestBundle package.
+ *
+ * (c) PrettyBundle <https://github.com/antwebes/SocialRestBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+$file = __DIR__.'/../vendor/autoload.php';
+$file_in_project = __DIR__ . '/../../../../../autoload.php';
 
-if (!is_file($autoloadFile)) {
-    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+if (!file_exists($file)) {
+    if (!file_exists($file_in_project)) {
+        throw new RuntimeException('Install dependencies to run test suite. Or add the bundle in project symfony2');
+    }else{
+        $file = $file_in_project;
+    }
 }
-
-require $autoloadFile;
+$autoload = require_once $file;
